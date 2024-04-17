b6ca60476201687a2658293489034ffcb1ce8e36
Roxana Rolón <113644811+RoxyDevs@users.noreply.github.com>
2024-04-17 19:45:02 -0300

    Add files via upload

---
+++ 04_PalabrasReservadas.py
@@ -0,0 +1,29 @@
+print("Python desde cero en Español")
+#--------------------------------------------------------------
+print("Author @RoxDev")
+print ("Puedes apoyar mis proyectos en:")
+
+base_url = "https://paypal.me/belakiss?country.x=AR&locale.x=es_XC"
+print(base_url)
+#--------------------------------------------------------------
+print("Tema: Palabras Reservadas: ")
+#--------------------------------------------------------------
+
+#* Palabras reservadass:
+#* Las palabras reservadas, son identificadores para uso
+#* exclusivo del lenguaje de programación, que no pueden 
+#* ser utilizadas para identificar y nombrar variables, métodos,
+#* objetos o cualquier elemento dentro de nuestro código. 
+
+#* Al intentar utilizar algunas de estas palabras reservadas 
+#* fuera del uso que tienen asignado, lo que obtendremos
+#* será un error de compilación y nuestro programa no podrá
+#* ser ejecutado.
+#--------------------------------------------------------------
+#REVIEW - Las siguientes son las palabras reservadas:
+
+#NOTE - and, del, for, is, raise, assert, if, else, elif, from, 
+#NOTE - lambda, return, break, global, not, try, class, except,
+#NOTE - or, while, continue, exec, import, yield, def, finally, 
+#NOTE - in, print
+#--------------------------------------------------------------
