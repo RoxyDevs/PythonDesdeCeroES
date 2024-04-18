f027465e6eed91407e6adb3bd6e615741d620639
Roxana Rolón <113644811+RoxyDevs@users.noreply.github.com>
2024-04-18 00:53:26 -0300

    Add files via upload

---
+++ 06_Comentarios.py
@@ -0,0 +1,34 @@
+print("Python desde cero en Español")
+#--------------------------------------------------------------
+print("Author @RoxDev")
+print ("Puedes apoyar mis proyectos en:")
+
+base_url = "https://paypal.me/belakiss?country.x=AR&locale.x=es_XC"
+print(base_url)
+#--------------------------------------------------------------
+print("Comentarios en Programación: ")
+#--------------------------------------------------------------
+
+#NOTE - En programación, un comentario es una construcción de 
+#NOTE - un lenguaje de programación destina a incrustar
+#NOTE - anotaciones legibles al programador en el código fuente
+#NOTE - de un programa.
+
+#NOTE - Estas anotaciones, son potencialmente significativas para 
+#NOTE - los programadores, pero usualmente ignorados por los
+#NOTE - compiladores e intérpretes.
+
+#NOTE - Los comentarios, son añadidos usualmente con el propósito
+#NOTE - de hacer el código mas fácil de entender con vistas a su
+#NOTE - mantenimiento o reutilización. 
+#NOTE - En Python la forma de añadir comentarios son las siguientes:
+#NOTE - Con el signo #, con un par de comillas "", o con triples
+#NOTE - comillas """, para crear comentarios múltilínea.abs
+
+#NOTE - La comillas NO son comentarios, son trings que, al no ser
+#NOTE - asignados a una variable, se interpretan como un valor
+#NOTE - nulo y por tal motivo el interprete de Python no los toma
+#NOTE - en cuenta. Con el cual se genera un comentario dentro del 
+#NOTE - Código.
+#--------------------------------------------------------------
+
