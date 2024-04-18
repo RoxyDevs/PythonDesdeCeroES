710ce45b428d32d2ece3d1bc340b15d77f388be2
Roxana Rolón <113644811+RoxyDevs@users.noreply.github.com>
2024-04-18 01:54:48 -0300

    Add files via upload

---
+++ 08_EntradaDeDatos1.py
@@ -0,0 +1,27 @@
+print("Python desde cero en Español")
+#--------------------------------------------------------------
+print("Author @RoxDev")
+print ("Puedes apoyar mis proyectos en:")
+
+base_url = "https://paypal.me/belakiss?country.x=AR&locale.x=es_XC"
+print(base_url)
+#--------------------------------------------------------------
+print("Entrada de Datos desde el teclado: ")
+#--------------------------------------------------------------
+
+#NOTE - Introduciremos datos a un programa desde el teclado
+#NOTE - La entrada de datos es importante para permitir que el
+#NOTE - usuario pueda interactuar con nuestros programas.
+#--------------------------------------------------------------
+
+palabra = input("Introduce una palabra: ")
+print("String: ", palabra)
+
+num_int = int(input("Ingrese un nro entero: "))
+print("Número entero: ", num_int)
+
+num_float = float(input("Introduce un nro flotante: "))
+print("Número flotante: ", num_float)
+
+num_complex = complex(input("Introduce un nro complejo: "))
+print("Número Complejo: ", num_complex)
---
+++ 08_EntradaDeDatos2.py
@@ -0,0 +1,10 @@
+#NOTE - Vamos solicitar el nombre del usuario
+nombre = input("¿Cuál es tu nombre?: ")
+print("Bienvenido/a " + nombre + ", vamos a realizar una suma. ")
+
+#NOTE - Suma
+num_uno = int(input("Ingrese el 1er valor: "))
+num_dos = int(input("Introduzca el 2ndo valor: "))
+resultado = num_uno + num_dos
+print("El resultado de la suma es: ", resultado)
+
